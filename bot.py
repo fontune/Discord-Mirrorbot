006258e01eb69a5f9a5c7d134e7aab3180091c0e
Matt Ford <sansfont@protonmail.com>
2019-06-29 00:37:05 +0100

    Added Embeds
    Messages are now output as embeds in the target channel

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import datetime
 
 # This is the token of the user account which has access to the channel where the messages
 # are being mirrored from
@@ -26,7 +27,6 @@ async def on_ready():
     print(userClient.user.id)
     print("-------")
 
-
 @botClient.event
 async def on_ready():
     print("Bot account connected")
@@ -34,19 +34,25 @@ async def on_ready():
     print(botClient.user.id)
     print("-------")
 
+def build_embed(authorName, authorPicture, embedTitle, embedDesc, embedColor):
+    emb = discord.Embed()
+    emb.set_author(name=authorName, url="", icon_url=authorPicture)
+    emb.title = embedTitle
+    emb.description = embedDesc
+    emb.color = embedColor
+    return emb
+
 @botClient.event
-async def mirror_message(messageString):
+async def send_message(messageEmbed):
     channel = botClient.get_channel(int(targetChannelID))
-    await channel.send(messageString)
+    await channel.send(embed=messageEmbed)
 
 @userClient.event
 async def on_message(message):
     if message.channel.id == int(sourceChannelID):
-        fullMessage = ""
-        fullMessage = fullMessage + message.author.name + "#"
-        fullMessage = fullMessage + message.author.discriminator + ": "
-        fullMessage = fullMessage + message.clean_content
-        await mirror_message(fullMessage)
+        authorName = message.author.name + "#" + message.author.discriminator
+        embedDesc = message.created_at.strftime("%H:%M:%S - %d/%m/%Y")
+        await send_message(build_embed(authorName, message.author.avatar_url, message.clean_content, embedDesc, message.author.color))
 
 loop = asyncio.get_event_loop()
 task1 = loop.create_task(userClient.start(userToken, bot=False))
