36df2de74f93bba5aa820db1c7c477b2d95cf1c3
Matt Ford <sansfont@protonmail.com>
2019-06-29 00:55:46 +0100

    Fixed message length error and removed timestamps
    - Message content is now handled in the description field, so no longer has a 256 character limit.
    
    - Removed the timestamp field as discord will already record this, messages are intended to be reposted live.

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import discord
 import asyncio
-import datetime
 
 # This is the token of the user account which has access to the channel where the messages
 # are being mirrored from
 userToken = ""
+
 # This is the token of the bot account, which can be created
 # at https://discordapp.com/developers/applications/
 botToken = ""
@@ -34,10 +34,9 @@ async def on_ready():
     print(botClient.user.id)
     print("-------")
 
-def build_embed(authorName, authorPicture, embedTitle, embedDesc, embedColor):
+def build_embed(authorName, authorPicture, embedDesc, embedColor):
     emb = discord.Embed()
     emb.set_author(name=authorName, url="", icon_url=authorPicture)
-    emb.title = embedTitle
     emb.description = embedDesc
     emb.color = embedColor
     return emb
@@ -51,8 +50,7 @@ async def send_message(messageEmbed):
 async def on_message(message):
     if message.channel.id == int(sourceChannelID):
         authorName = message.author.name + "#" + message.author.discriminator
-        embedDesc = message.created_at.strftime("%H:%M:%S - %d/%m/%Y")
-        await send_message(build_embed(authorName, message.author.avatar_url, message.clean_content, embedDesc, message.author.color))
+        await send_message(build_embed(authorName, message.author.avatar_url, message.clean_content, message.author.color))
 
 loop = asyncio.get_event_loop()
 task1 = loop.create_task(userClient.start(userToken, bot=False))
